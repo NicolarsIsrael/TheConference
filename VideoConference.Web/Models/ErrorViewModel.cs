fffdc6aec89964c14c8cb3a855eea06c67b4d669
NicolarsIsrael <bakareisrael@gmail.com>
2020-07-17 12:13:38 +0100

    facebook and google login btn design

---
+++ VideoConference.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VideoConference.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
