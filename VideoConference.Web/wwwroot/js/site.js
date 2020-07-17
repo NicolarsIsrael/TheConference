011b1301ce9d6cdff7f04c52839d1e0d2432a5e1
NicolarsIsrael <bakareisrael@gmail.com>
2020-07-17 12:48:44 +0100

    show password function

--- VideoConference.Web/wwwroot/js/site.js
+++ VideoConference.Web/wwwroot/js/site.js
@@ -2,3 +2,16 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function ShowPassword() {
+    var passwordInput = document.getElementById("passwordInput");
+    var passwordLockIcon = document.getElementById("password-lock-icon");
+
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+    } else {
+        passwordInput.type = "password";
+    }
+
+    passwordLockIcon.classList.toggle("fa-eye-slash");
+}
